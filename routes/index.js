8ca22a06cc82741932a0f394f7dffc2d7795a2ca
Jiyang Wang <jiyang.wang.us@gmail.com>
2016-09-05 10:37:12 -0700

    add upload

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,19 @@ var crypto = require('crypto'),
     User = require('../models/user.js'),
     Post = require('../models/post.js');
 
+var multer  = require('multer');
+var storage = multer.diskStorage({
+    destination: function (req, file, cb){
+        cb(null, './public/images')
+    },
+    filename: function (req, file, cb){
+        cb(null, file.originalname)
+    }
+});
+var upload = multer({
+    storage: storage
+});
+
 module.exports = function(app) {
   app.get('/', function (req, res) {
   Post.get(null, function (err, posts) {
@@ -131,6 +144,22 @@ module.exports = function(app) {
     req.session.user = null;
     req.flash('success', 'You have successfully logout!');
     res.redirect('/');//return to main page
+  });
+
+  app.get('/upload', checkLogin);
+  app.get('/upload', function (req, res) {
+    res.render('upload', {
+      title: 'Upload',
+      user: req.session.user,
+      success: req.flash('success').toString(),
+      error: req.flash('error').toString()
+    });
+  });
+
+  app.post('/upload', checkLogin);
+  app.post('/upload', upload.array('field1', 5), function (req, res) {
+    req.flash('success', 'Upload successfully!');
+    res.redirect('/upload');
   });
 
   function checkLogin(req, res, next) {
